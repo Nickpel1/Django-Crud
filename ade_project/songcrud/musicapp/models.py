e4ce81dcaf4b9203f5b6f87966e82415b0eee403
Nickpel1 <bimpeadekunle03@gmail.com>
2022-11-04 08:52:00 -0700

    all files updated

--- ade_project/songcrud/musicapp/models.py
+++ ade_project/songcrud/musicapp/models.py
@@ -8,15 +8,26 @@ class Artiste(models.Model):
     last_name =models.CharField(max_length= 60)
     age = models.IntegerField()
 
-class Song(models.Model):
-    Artiste = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
 
+class Song(models.Model):
     title = models.CharField(max_length= 100)
     date_released = models.DateField(default= datetime.today)
     likes = models.IntegerField()
+    artiste_id = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.title}"
 
 class Lyric(models.Model):
-    Song = models.ForeignKey(Song, on_delete=models.CASCADE)
+    content = models.TextField()
+    song_id = models.ForeignKey(Song, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        if len(self.content) > 50:
+            return f"{self.content[0:50]}..."
+        else:
+            return f"{self.content}"
 
-    content =models.CharField(max_length= 100)
 
